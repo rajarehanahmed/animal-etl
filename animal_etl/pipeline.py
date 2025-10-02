6742567bbec136c879d80d0d16ebdb16f2d1821f
Rehan <rajarehan.ahmed@taleemabad.com>
2025-10-03 00:34:07 +0500

    feat: transform and load

--- animal_etl/pipeline.py
+++ animal_etl/pipeline.py
@@ -3,6 +3,7 @@ import sys
 from typing import Any
 
 from animal_etl.api_client import APIClient
+from animal_etl.transformers import batch_animals, transform_animal
 
 logging.basicConfig(
     level=logging.INFO,
@@ -30,21 +31,43 @@ class AnimalETLPipeline:
 
             # Get detailed info for each animal
             for animal_summary in animals_on_page:
+                # it could get slow if there is too much data, we could optimize this by sending multiple api calls
+                # asynchronously, but as the data size is not that much and the server
+                # is also slow (accordinng to the doc), we can keep it simple for now
                 animal_detail = self.api_client.get(endpoint=f"{endpoint}/{animal_summary['id']}")
                 logger.info(f"Animal ID {animal_detail['id']}: {animal_detail['name']}")
                 all_animals.append(animal_detail)
 
-            if page >= total_pages:
+            if page >= 10:
                 break
             page += 1
 
         logger.info(f"Fetched {len(all_animals)} animals from {page} pages")
         return all_animals
 
+    def transform_animals(self, animals: list[dict[str, Any]]) -> list[dict[str, Any]]:
+        return [transform_animal(animal) for animal in animals]
+
+    def load_animals(self, animals: list[dict[str, Any]], batch_size: int = 100) -> None:
+        endpoint = "/animals/v1/home"
+        batches = batch_animals(animals, batch_size)
+
+        for i, batch in enumerate(batches, 1):
+            self.api_client.post(endpoint=endpoint, data=batch)
+            logger.info(f"Loaded batch {i}/{len(batches)}")
+
     def run(self) -> None:
         logger.info("Starting ETL pipeline")
 
+        logger.info("Extracting animals from API")
         animals = self.fetch_animals()
+
+        logger.info("Transforming animals")
+        transformed_animals = self.transform_animals(animals)
+        logger.info(transformed_animals)
+
+        logger.info("Loading animals to API")
+        self.load_animals(transformed_animals)
 
 
 if __name__ == "__main__":
---
+++ animal_etl/transformers.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timezone
+from typing import Any
+from logging import getLogger
+
+
+logger = getLogger(__name__)
+
+
+def transform_animal(animal: dict[str, Any]) -> dict[str, Any]:
+    born_at = transform_timestamp(animal["born_at"]) if animal.get("born_at") else None
+    friends = transform_friends(animal["friends"])
+
+    return {
+        "id": animal["id"],
+        "name": animal["name"],
+        "friends": friends,
+        **({"born_at": born_at} if born_at else {}),
+    }
+
+
+def transform_friends(friends_str: str) -> list[str]:
+    if not friends_str or friends_str.strip() == "":
+        return []
+    return [friend.strip() for friend in friends_str.split(",") if friend.strip()]
+
+
+def transform_timestamp(timestamp: int) -> str | None:
+    try:
+        # timestamp is in milliseconds, so convert to seconds before processing
+        time_stamp = timestamp / 1000.0
+
+        dt = datetime.fromtimestamp(time_stamp, tz=timezone.utc)
+        return dt.isoformat()
+    except Exception as e:
+        logger.error(f"Error transforming timestamp {timestamp}: {e}")
+        return None
+
+
+def batch_animals(animals: list[dict[str, Any]], batch_size: int = 100) -> list[list[dict[str, Any]]]:
+    return [animals[i : i + batch_size] for i in range(0, len(animals), batch_size)]
